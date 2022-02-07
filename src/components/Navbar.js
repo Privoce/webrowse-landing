c900a5d19a714428847db1625e43528eaebfa9e4
zerosoul <yanggc888@163.com>
2022-02-07 17:54:05 +0800

    feat: add blog nav link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react"
-import styled from "styled-components"
-import { AuthingGuard } from '@authing/react-ui-components'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { AuthingGuard } from "@authing/react-ui-components";
 // 引入 css 文件
-import '@authing/react-ui-components/lib/index.min.css';
-import GuardConfig, { appId } from '../constants/guardConfig';
-import Logo from '../images/logo.png';
+import "@authing/react-ui-components/lib/index.min.css";
+import GuardConfig, { appId } from "../constants/guardConfig";
+import Logo from "../images/logo.png";
 
-const Navbar = ({ curr = 'home', updateUser = null }) => {
+const Navbar = ({ curr = "home", updateUser = null }) => {
   const [user, setUser] = useState(null);
   const [guardVisible, setGuardVisible] = useState(false);
   // 及时地同步给父组件
   useEffect(() => {
-    updateUser && updateUser(user)
+    updateUser && updateUser(user);
   }, [user]);
   const handleGuardLoad = async (authClient) => {
     let currUser = await authClient.getCurrentUser();
@@ -23,7 +23,7 @@ const Navbar = ({ curr = 'home', updateUser = null }) => {
     }
   };
   const handleGuardLogin = async (user) => {
-    console.log('login', { user });
+    console.log("login", { user });
     setUser(user);
     setGuardVisible(false);
   };
@@ -35,8 +35,8 @@ const Navbar = ({ curr = 'home', updateUser = null }) => {
     setGuardVisible(false);
   };
   const handleLogin = () => {
-    setGuardVisible(true)
-  }
+    setGuardVisible(true);
+  };
   return (
     <>
       <AuthingGuard
@@ -58,138 +58,167 @@ const Navbar = ({ curr = 'home', updateUser = null }) => {
           <button className="btn">Get Started</button>
         </div>
         <div className="head">
-
           <div className="left">
             <img src={Logo} className="logo" alt="webrowse logo" />
           </div>
           <div className="middle">
-            <a className={`link ${curr == 'home' ? 'curr' : ""}`} href="/#home" >Home</a>
-            <a className={`link ${curr == 'pricing' ? 'curr' : ""}`} href="/pricing" >Pricing</a>
-            <a className={`link`} href="https://discord.gg/2wKtHnXMVV" target="_blank" >Discord</a>
+            <a className={`link ${curr == "home" ? "curr" : ""}`} href="/#home">
+              Home
+            </a>
+            <a
+              className={`link ${curr == "pricing" ? "curr" : ""}`}
+              href="/pricing"
+            >
+              Pricing
+            </a>
+            <a
+              className={`link`}
+              href="https://discord.gg/2wKtHnXMVV"
+              target="_blank"
+            >
+              Discord
+            </a>
+            <a className={`link`} href="/blog/" target="_blank">
+              Blog
+            </a>
           </div>
           <div className="right">
-            {user ? <a className="btn login" href="https://portal-china.authing.cn/u">{user.username}</a> : <button className="btn login" onClick={handleLogin}>Log In</button>}
-            <a href="https://calendly.com/hansu/han-meeting" target="_blank" className="btn typeform">Request Demo</a>
+            {user ? (
+              <a className="btn login" href="https://portal-china.authing.cn/u">
+                {user.username}
+              </a>
+            ) : (
+              <button className="btn login" onClick={handleLogin}>
+                Log In
+              </button>
+            )}
+            <a
+              href="https://calendly.com/hansu/han-meeting"
+              target="_blank"
+              className="btn typeform"
+            >
+              Request Demo
+            </a>
             {/* <a className="btn add" target="_blank" href="https://chrome.google.com/webstore/detail/webrowse-sync-tabs-with-y/nnbkebemeehfhiimeghnkdocfbeogenn/related">Add to Chrome</a> */}
           </div>
         </div>
       </NavStyles>
     </>
-  )
-}
+  );
+};
 
 const NavStyles = styled.nav`
-    position: absolute;
-    left: 0;
-    top:0;
-    width: 100%;
-    .coupon{
-      background: #212026;
-      padding:16px 110px;
-      display: none;
-      justify-content: space-between;
-      align-items: center;
-      gap: 35px;
-      @media screen and (max-width: 1100px) {
-        padding:16px 20px;
-        }
-      .intro{
-        justify-content: space-between;
-        display: flex;
-        width: 100%;
-        color: #fff;
-        font-style: normal;
-        font-weight: 600;
-        font-size: 24px;
-        line-height: 38px;
-      }
-      .btn{
-        cursor: pointer;
-        white-space: nowrap;
-        background: none;
-        font-weight: 500;
-        font-size: 22px;
-        line-height: 31px;
-        color: #18BFFF;
-        padding: 4px 8px;
-        border-radius: 8px;
-        border: 3px solid #18BFFF;
-      }
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  .coupon {
+    background: #212026;
+    padding: 16px 110px;
+    display: none;
+    justify-content: space-between;
+    align-items: center;
+    gap: 35px;
+    @media screen and (max-width: 1100px) {
+      padding: 16px 20px;
     }
-    .head{
+    .intro {
+      justify-content: space-between;
+      display: flex;
+      width: 100%;
+      color: #fff;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 24px;
+      line-height: 38px;
+    }
+    .btn {
+      cursor: pointer;
+      white-space: nowrap;
+      background: none;
+      font-weight: 500;
+      font-size: 22px;
+      line-height: 31px;
+      color: #18bfff;
+      padding: 4px 8px;
+      border-radius: 8px;
+      border: 3px solid #18bfff;
+    }
+  }
+  .head {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: transparent;
+    padding: 15px 110px;
+    .left {
       display: flex;
       align-items: center;
-      justify-content: space-between;
-      background: transparent;
-      padding:15px 110px;
-      .left{  
+      .logo {
+        width: 203px;
+        height: 36px;
+      }
+      .title {
+        text-transform: capitalize;
+        font-size: 20px;
+        line-height: 25px;
+        color: #fff;
+        margin-right: 32px;
+      }
+    }
+    .middle {
+      display: flex;
+      gap: 20px;
+      .link {
+        color: #787878;
+        font-size: 14px;
+        line-height: 18px;
+        text-decoration: none;
+        &:hover {
+          color: #056cf2;
+        }
+        &.curr {
+          border-bottom: 1px solid #787877;
+        }
+      }
+    }
+    .right {
+      display: flex;
+      gap: 16px;
+      font-size: 16px;
+      @media screen and (max-width: 414px) {
+        display: none;
+      }
+      .btn {
+        cursor: pointer;
+        text-decoration: none;
+        border-radius: 50px;
+        background: #52edff;
+        border: none;
+        font-size: 14px;
+        font-weight: bold;
+        padding: 12px 24px;
+        color: #000;
         display: flex;
         align-items: center;
-        .logo{
-          width: 203px;
-          height: 36px;
-        }
-        .title{
-          text-transform: capitalize;
-          font-size: 20px;
-          line-height: 25px;
-          color: #fff;
-          margin-right: 32px;
-        }
-      }        
-      .middle{
-          display: flex;
-          gap:20px;
-          .link{
-            color:#787878;
-            font-size: 14px;
-            line-height: 18px;
-            text-decoration: none;
-            &:hover{
-              color: #056CF2;
-            }
-            &.curr{
-              border-bottom: 1px solid #787877;
-            }
-          }
-      }
-      .right{
-        display: flex;
-        gap:16px;
-        font-size: 16px;
-        @media screen and (max-width: 414px) {
-          display: none;
-        }
-        .btn{
-          cursor: pointer;
-          text-decoration: none;
-          border-radius: 50px;
-          background: #52EDFF;
-          border:none;
-          font-size: 14px;
-          font-weight: bold;
-          padding: 12px 24px;
-          color: #000;
-          display: flex;
-          align-items: center;
-          
-          &.login{
-            background: none;
-            color:#52EDFF;
-            border:2px solid #52EDFF
-          }
-        }
-      }
-      @media screen and (max-width: 414px) {
-        padding:15px 10px;
-        .left {
-          .logo {
-            width: 160px;
-            height: auto;
-          }
-        }
-      }
-  }
-`
 
-export default Navbar
+        &.login {
+          background: none;
+          color: #52edff;
+          border: 2px solid #52edff;
+        }
+      }
+    }
+    @media screen and (max-width: 414px) {
+      padding: 15px 10px;
+      .left {
+        .logo {
+          width: 160px;
+          height: auto;
+        }
+      }
+    }
+  }
+`;
+
+export default Navbar;
