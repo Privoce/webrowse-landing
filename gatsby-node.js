23cc1e6801b6e91677203c8e56f9fe3f295952ce
shilei <laoona@163.com>
2022-03-25 15:37:49 +0800

    Revert "Feat/voice channel"

--- gatsby-node.js
+++ gatsby-node.js
@@ -113,18 +113,3 @@ exports.createSchemaCustomization = ({ actions }) => {
     }
   `)
 }
-
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /agora-rtc-sdk-ng/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
